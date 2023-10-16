50fbcbe0c75ab8e4653e3786983da8b29837b5de
taisan111111 <tai.kappy55@gmail.com>
2023-10-16 21:54:21 +0900

    🎉 うぇい

---
+++ notify.ts
@@ -0,0 +1,39 @@
+import ky from "https://cdn.skypack.dev/ky?dts";
+const url = "https://notify-api.line.me/api/notify";
+
+export async function lineNotify(
+  params: {
+    message: string;
+    token: string;
+    imageThumbnailURL?: string;
+    imageFullsizeURL?: string;
+  },
+) {
+  const { message, token, imageThumbnailURL, imageFullsizeURL } = params;
+
+  if (!message || !token) {
+    throw new Error("Missing message or token");
+  }
+
+  const body = new URLSearchParams();
+  body.set("message", message);
+  if (imageThumbnailURL) {
+    // validate url
+    new URL(imageThumbnailURL);
+    body.set("imageThumbnail", imageThumbnailURL);
+  }
+  if (imageFullsizeURL) {
+    // validate url
+    new URL(imageFullsizeURL);
+    body.set("imageFullsize", imageFullsizeURL);
+  }
+
+  try {
+    const headers = { Authorization: `Bearer ${token}` };
+    return await ky.post(url, { headers, body }).json();
+  } catch (error) {
+    console.error(`${error}`);
+    return error;
+    return await error.response.json();
+  }
+}
