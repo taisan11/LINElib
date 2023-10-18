e2e65a443f942ca37755ebd7029057872daeb075
taisan111111 <tai.kappy55@gmail.com>
2023-10-18 16:27:32 +0900

    iroiro

--- notify_oauth.ts
+++ notify_oauth.ts
@@ -6,27 +6,27 @@ const urlm: string = "https://notify-bot.line.me/oauth/authorize";
 // ?response_type=code&client_id=[client_id]&redirect_uri=[redirect_uri]&scope=notify&state=[state]
 
 export async function lineNotify(
-    params: {
-      clientid: string;
-      redirect_uri: string;
-      state: string;
-    
-    },
-  ) {
-    const { clientid, redirect_uri ,state } = params;
-    // 内容とトークンcheck
-    if (!clientid || !redirect_uri || !state) {
-        
-        throw new Error("Required fields are missing.");
-    }
-  
-    const body = new URLSearchParams();
-    const url: string = urlm + "?response_type=code&client_id=" + clientid + "&redirect_uri=" + redirect_uri + "&scope=notify&state=" + state;
-    try {
-      return await ky.get(url, { body }).json();
-    } catch (error) {
-      log.error(`${error}`);
-      return error;
-      return await error.response.json();
-    }
-  }  
+  params: {
+    clientid: string;
+    clientsecret: string;
+    redirect_uri: string;
+    state: string;
+  },
+) {
+  const { clientid, redirect_uri, state, clientsecret } = params;
+  // 内容とトークンcheck
+  if (!clientid || !redirect_uri || !state) {
+    throw new Error("Required fields are missing.");
+  }
+
+  const body = new URLSearchParams();
+  const url: string = urlm + "?response_type=code&client_id=" + clientid +
+    "&redirect_uri=" + redirect_uri + "&scope=notify&state=" + state;
+  try {
+    return await ky.get(url, { body }).json();
+  } catch (error) {
+    log.error(`${error}`);
+    return error;
+    return await error.response.json();
+  }
+}
--- test/play.ts
+++ test/play.ts
@@ -2,5 +2,11 @@ import ky from "https://cdn.skypack.dev/ky?dts";
 import { Log } from "https://pax.deno.dev/kawarimidoll/deno-tl-log";
 
 const log = new Log();
-
-ky.post("https://notify-api.line.me/api/notify").json
+log.info("OK");
+// ky.get("https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=XXX&redirect_uri=XXX&scope=notify&state=NO_STATE")
+// https://fair-swan-75.deno.dev/?code=XXX&state=NO_STATEの形で返される
+// const url: string = "https://notify-bot.line.me/oauth/token?grant_type=authorization_code&code=XXX&redirect_uri=XXX&client_id=XXXX&client_secret=XXXX"
+log.info(
+  ky.post("https://example.com"
+  ).json(),
+);
