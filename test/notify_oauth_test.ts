070b920891114483316e5d54c198b7d30b5a345a
taisan111111 <tai.kappy55@gmail.com>
2023-10-19 18:31:19 +0900

    haxtu?

--- test/notify_oauth_test.ts
+++ test/notify_oauth_test.ts
@@ -1,6 +1,7 @@
 // import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { lineNotifyoAuth } from "../notify_oauth.ts";
+import { lineNotifyoAuth, lineNotifyToken } from "../notify_oauth.ts";
 import { Log } from "https://pax.deno.dev/kawarimidoll/deno-tl-log";
+import { open } from "https://deno.land/x/open@v0.0.6/index.ts";
 import { clientid, clientsecret, redirect_uri } from "./env.ts";
 
 const log = new Log();
@@ -14,4 +15,18 @@ const Aaa = lineNotifyoAuth({
   redirect_uri: redirect_uri,
   state: "test",
 });
-console.log(Aaa);
+await open(Aaa);
+
+//codeの読み込み
+console.log("%cコードを取得してください%cやり方は下をどうぞ", "color:red");
+console.log("Please enter the value of the 'code' parameter in the URL, which should be in the format '?code=xxxx'.");
+const code = prompt("Please enter your code:");
+console.log("code:", code);
+if (code !== null) {
+  lineNotifyToken({
+    clientid: clientid,
+    clientsecret: clientsecret,
+    redirect_uri: redirect_uri,
+    code: code,
+  });
+}
